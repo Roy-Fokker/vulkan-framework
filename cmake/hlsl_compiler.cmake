b4ab4ccbf2d85a24fbd913e8789ce0a79788ea6d
Roy-Fokker <11281115+Roy-Fokker@users.noreply.github.com>
2024-06-09 20:34:42 -0400

    seems we can make do without checking profile value, dxc will handle it

--- cmake/hlsl_compiler.cmake
+++ cmake/hlsl_compiler.cmake
@@ -17,18 +17,18 @@ function(target_hlsl_sources TARGET)
 	list(LENGTH ARGN count_HLSL)
 	math(EXPR count_HLSL "${count_HLSL} / 3")
 
-	# List of profile names supported by LunarG supplied dxc
-	set(valid_profiles 
-		ps_6_0 ps_6_1 ps_6_2 ps_6_3 ps_6_4 ps_6_5 ps_6_6 ps_6_7 ps_6_8 
-		vs_6_0 vs_6_1 vs_6_2 vs_6_3 vs_6_4 vs_6_5 vs_6_6 vs_6_7 vs_6_8 
-		gs_6_0 gs_6_1 gs_6_2 gs_6_3 gs_6_4 gs_6_5 gs_6_6 gs_6_7 gs_6_8 
-		hs_6_0 hs_6_1 hs_6_2 hs_6_3 hs_6_4 hs_6_5 hs_6_6 hs_6_7 hs_6_8 
-		ds_6_0 ds_6_1 ds_6_2 ds_6_3 ds_6_4 ds_6_5 ds_6_6 ds_6_7 ds_6_8 
-		cs_6_0 cs_6_1 cs_6_2 cs_6_3 cs_6_4 cs_6_5 cs_6_6 cs_6_7 cs_6_8 
-		lib_6_1 lib_6_2 lib_6_3 lib_6_4 lib_6_5 lib_6_6 lib_6_7 lib_6_8 
-		ms_6_5 ms_6_6 ms_6_7 ms_6_8 
-		as_6_5 as_6_6 as_6_7 as_6_8
-	)
+	# # List of profile names supported by LunarG supplied dxc
+	# set(valid_profiles 
+	# 	ps_6_0 ps_6_1 ps_6_2 ps_6_3 ps_6_4 ps_6_5 ps_6_6 ps_6_7 ps_6_8 
+	# 	vs_6_0 vs_6_1 vs_6_2 vs_6_3 vs_6_4 vs_6_5 vs_6_6 vs_6_7 vs_6_8 
+	# 	gs_6_0 gs_6_1 gs_6_2 gs_6_3 gs_6_4 gs_6_5 gs_6_6 gs_6_7 gs_6_8 
+	# 	hs_6_0 hs_6_1 hs_6_2 hs_6_3 hs_6_4 hs_6_5 hs_6_6 hs_6_7 hs_6_8 
+	# 	ds_6_0 ds_6_1 ds_6_2 ds_6_3 ds_6_4 ds_6_5 ds_6_6 ds_6_7 ds_6_8 
+	# 	cs_6_0 cs_6_1 cs_6_2 cs_6_3 cs_6_4 cs_6_5 cs_6_6 cs_6_7 cs_6_8 
+	# 	lib_6_1 lib_6_2 lib_6_3 lib_6_4 lib_6_5 lib_6_6 lib_6_7 lib_6_8 
+	# 	ms_6_5 ms_6_6 ms_6_7 ms_6_8 
+	# 	as_6_5 as_6_6 as_6_7 as_6_8
+	# )
 
 	# Loop through all the pairs for filename:profile provided
 	foreach(i RANGE 1 ${count_HLSL})
@@ -37,11 +37,11 @@ function(target_hlsl_sources TARGET)
 		list(GET arg_HLSL ${fni} hlsl_filename)
 		list(GET arg_HLSL ${pfi} hlsl_profile)
 
-		# check the profile name matches the above list.
-		list(FIND valid_profiles ${hlsl_profile} found_profile)
-		if(found_profile EQUAL -1)
-			message(FATAL_ERROR "Profile ${hlsl_profile} for ${hlsl_filename} is not recognized.")
-		endif()
+		# # check the profile name matches the above list.
+		# list(FIND valid_profiles ${hlsl_profile} found_profile)
+		# if(found_profile EQUAL -1)
+		# 	message(FATAL_ERROR "Profile ${hlsl_profile} for ${hlsl_filename} is not recognized.")
+		# endif()
 
 		# get the absolute path of current source file
 		file(REAL_PATH ${hlsl_filename} source_abs)
