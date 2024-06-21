757cc44d6cd89c9f6c4f7b5a85c795d6c22f34e1
Roy-Fokker <11281115+Roy-Fokker@users.noreply.github.com>
2024-06-20 20:24:01 -0400

    hlsl match glsl

--- src/shaders/basic_compute.hlsl
+++ src/shaders/basic_compute.hlsl
@@ -1,9 +1,11 @@
 
+// descriptor bindings for the pipeline
 [[vk::image_format("rgba16f")]]
 RWTexture2D<float4> image : register(u0, space0);
 
-[numthreads(16, 16, 1)]
-void main(uint3 thread_id : SV_DispatchThreadID)
+[numthreads(16, 16, 1)] // size of a workgroup for compute
+void main(uint3 thread_id : SV_DispatchThreadID,
+          uint3 local_id : SV_GroupThreadID)
 {
 	int2 texel_coord = (int2)thread_id.xy;
 	int2 size;
@@ -12,7 +14,7 @@ void main(uint3 thread_id : SV_DispatchThreadID)
 	if (texel_coord.x < size.x && texel_coord.y < size.y)
 	{
 		float4 color = float4(0.0, 0.0, 0.0, 1.0);
-		if (thread_id.x != 0 && thread_id.y != 0)
+		if (local_id.x != 0 && local_id.y != 0)
 		{
 			color.x = float(texel_coord.x) / (size.x);
 			color.y = float(texel_coord.y) / (size.y);
